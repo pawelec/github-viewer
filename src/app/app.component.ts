b3156435754c316ee93c343d54916b247e9c5954
Paweł Pawelec <kontakt@ppawelec.pl>
2017-09-22 15:07:36 +0200

    Add search bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName: string;
+
+  nameChanged(name: string) {
+    this.userName = name;
+  }
 }
---
+++ src/app/searchBar/search-bar.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+
+export class SearchBarComponent implements OnInit {
+  searchForm: FormGroup;
+  @Output() nameChanged = new EventEmitter<string>();
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      name: [null, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if(this.searchForm.invalid) {
+      this.searchForm.markAsTouched();
+      this.searchForm.updateValueAndValidity();
+      return;
+    }
+    this.nameChanged.emit(this.searchForm.controls['name'].value);
+  }
+}
