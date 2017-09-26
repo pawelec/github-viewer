16de3c757ee2c16644c25e3a3f554b831351fbc0
pafcio <pawel.pawelec@bankmillennium.pl>
2017-09-26 07:31:47 +0200

    Add empty About component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
     { path: 'users/:name', component: UsersComponent },
+    { path: 'about', component: AboutComponent},
     { path: '**', redirectTo: '/' }
 ];
 
